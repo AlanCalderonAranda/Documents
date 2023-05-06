d1f8dc48bea3bdf30e472b386b426965dc53a8c6
Alan Calderon <alancalderonaranda@gmail.com>
2023-05-06 01:43:11 -0600

    first commit

---
+++ NetBeansProjects/clinica_Veterinaria/src/main/java/mx/uacm/edu/mx/clinica_veterinaria/Clinica_Veterinaria.java
@@ -0,0 +1,12 @@
+package mx.uacm.edu.mx.clinica_veterinaria;
+
+/**
+ *
+ * @author AlanCalderon
+ */
+public class Clinica_Veterinaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
